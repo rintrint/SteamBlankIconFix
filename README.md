42af23fd860aa7847087754175ce5f7d3db629e1
rintrint <152774318+rintrint@users.noreply.github.com>
2026-05-12 18:26:52 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# SteamBlankIconFix
+
+修復Steam遊戲圖示變成空白的問題。
+
+## 步驟
+
+1. 運行 script/1_fix_registry.reg
+2. 運行 script/2_clear_thumbnail_cache.bat
+
+運行完兩個腳本後觀察問題是否已經解決。
+
+## 其他方法
+
+還有一種方法是將Steam用的.url捷徑轉換成.lnk捷徑，比.url捷徑更穩定。  
+<https://github.com/vixark/Url-Shortcut-to-Lnk-Converter>  
+
+## Reference
+
+<https://github.com/ez4y2f/SteamIconFix>  
+<https://superuser.com/q/1310021/903729>  
+<https://www.winhelponline.com/blog/file-association-fixes-windows-10/>  
---
+++ script/2_clear_thumbnail_cache.bat
@@ -0,0 +1,17 @@
+taskkill /f /im explorer.exe
+attrib -h -s -r "%userprofile%\AppData\Local\IconCache.db"
+del /f "%userprofile%\AppData\Local\IconCache.db"
+
+attrib /s /d -h -s -r "%userprofile%\AppData\Local\Microsoft\Windows\Explorer\*"
+del /f "%userprofile%\AppData\Local\Microsoft\Windows\Explorer\thumbcache_32.db"
+del /f "%userprofile%\AppData\Local\Microsoft\Windows\Explorer\thumbcache_96.db"
+del /f "%userprofile%\AppData\Local\Microsoft\Windows\Explorer\thumbcache_102.db"
+del /f "%userprofile%\AppData\Local\Microsoft\Windows\Explorer\thumbcache_256.db"
+del /f "%userprofile%\AppData\Local\Microsoft\Windows\Explorer\thumbcache_1024.db"
+del /f "%userprofile%\AppData\Local\Microsoft\Windows\Explorer\thumbcache_idx.db"
+del /f "%userprofile%\AppData\Local\Microsoft\Windows\Explorer\thumbcache_sr.db"
+
+reg delete "HKCU\Software\Classes\Local Settings\Software\Microsoft\Windows\CurrentVersion\TrayNotify" /v IconStreams /f
+reg delete "HKCU\Software\Classes\Local Settings\Software\Microsoft\Windows\CurrentVersion\TrayNotify" /v PastIconsStream /f
+
+start explorer.exe
